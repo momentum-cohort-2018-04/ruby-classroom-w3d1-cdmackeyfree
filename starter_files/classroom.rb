84902a837f3b8abdbe99e06e425f7c5b582ac04a
Crystal Free <cdmackeyfree@gmail.com>
2018-05-08 15:27:23 -0400

    method 3 passing

--- starter_files/classroom.rb
+++ starter_files/classroom.rb
@@ -20,10 +20,18 @@ def assignment_scores(hash, assign_num)
     end  
 end
 
-
 # Given a grade_hash and assignment number, return the average score for that
 # assignment. Note that Ruby counts arrays from 0, but we are referring to
 # them as 1-10.
+
+def assignment_average_score(hash, assign_num)
+    #call assignment_scores to get an array of the scores using the args
+    grade_array = assignment_scores(hash, assign_num)
+    #calculate and return the average of the array
+    sum = grade_array.reduce(0) { |x, y| x + y }
+    average = sum/grade_array.length
+
+end
 
 
 # Return a hash of students and their average score.
